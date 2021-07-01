0dc998fd4fa22964bd53400038d5987c272af4a9
Brindha <brindhashree2008@gmail.com>
2021-06-30 22:08:21 -0400

    Initial commit

---
+++ test1.js
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="C:\Users\brind\OneDrive\Desktop\Python_Work\projectcircleci.js"></script>
+var count = 1;
+document.getElementById('button').onclick = RndMsg() 
+   alert("button was clicked " + (count++) + " times");
