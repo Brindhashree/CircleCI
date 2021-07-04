d275dd1cf743fccaf06ecf8066ef68a710b7c3f1
Brindha <brindhashree2008@gmail.com>
2021-07-04 17:14:44 -0400

    commit with all files

---
+++ README.md
@@ -0,0 +1,12 @@
+I've used Jest and @testing-library/react alongside for testing my vanilla javascript based code.
+
+Development
+If you'd like to run this project locally, clone the repo and then:
+
+yarn install (or npm install) to install dependencies
+yarn start (or npm start) to preview the app locally
+yarn test (or npm test) to run the tests
+
+---------------------------------------------------------------
+
+Docker_Challenge.md containes the responses for the docker challenges
---
+++ RndMsg.test.js
@@ -0,0 +1,37 @@
+import { fireEvent, getByText } from '@testing-library/dom'
+import '@testing-library/jest-dom/extend-expect'
+import { JSDOM } from 'jsdom'
+import fs from 'fs'
+import path from 'path'
+
+const html = fs.readFileSync(path.resolve(__dirname, './RndMsg.html'), 'utf8');
+let dom
+let container
+
+describe('RndMsg.html', () => {
+  beforeEach(() => {
+    // Constructing a new JSDOM with this option is the key
+    // to getting the code in the script tag to execute.
+    // This is indeed dangerous and should only be done with trusted content.
+    // https://github.com/jsdom/jsdom#executing-scripts
+    dom = new JSDOM(html, { runScripts: 'dangerously' })
+    container = dom.window.document.body
+  })
+
+  
+
+  it('renders a button element', () => {
+    expect(container.querySelector('button')).not.toBeNull()
+    expect(getByText(container, 'Click Me')).toBeInTheDocument()
+  })
+
+  it('renders a new paragraph via JavaScript when the button is clicked', async () => {
+    const button = getByText(container, 'Click Me')
+    
+    fireEvent.click(button)
+    let generatedParagraphs = container.querySelectorAll('#pun-container p')
+    expect(generatedParagraphs.length).toBe(1)
+
+    
+  })
+})
